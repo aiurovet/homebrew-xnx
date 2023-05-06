22324aa529f95a539e94754fe28e9dae2d1693a2
aiurovet <aiurovet@gmail.com>
2023-05-07 04:08:25 +1000

    WIP

--- xnx.rb
+++ xnx.rb
@@ -8,7 +8,7 @@ class Xnx < Formula
   bottle do
     if OS.linux?
       root_url "https://github.com/aiurovet/xnx/raw/master/app/Linux"
-      sha256 x86_64_linux: "afa9930d72151e4d558153f24f6d4f1b2ec86a74403b47eaa80af61f882c44be"
+      sha256 x86_64_linux: "6af1847d443fbecc210619e457cd603012014535f8851184c0fbecdf2751ff95"
     elsif OS.mac?
       root_url "https://github.com/aiurovet/xnx/raw/master/app/macOS"
       sha256 big_sur: "9829601feda4401a92f5587cfa6e9f7b40b1e4f71e6152a3934bca7ba9f3309a"
