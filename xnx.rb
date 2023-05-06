fa1f3be756e5d1e68433cbe6ed9d84a1a3d7c248
aiurovet <aiurovet@gmail.com>
2023-05-06 23:41:03 +1000

    WIP

--- xnx.rb
+++ xnx.rb
@@ -1,7 +1,7 @@
 class Xnx < Formula
   desc "Command-line utility for sophisticated search and replace followed by calling external executables"
   homepage "https://aiurovet.com/applications/xnx.html"
-  url "https://github.com/aiurovet/xnx/raw/"
+  url "https://github.com/aiurovet/xnx"
   version "0.1.0"
   license "MIT"
 
@@ -11,18 +11,11 @@ class Xnx < Formula
       sha256 x86_64_linux: "6af1847d443fbecc210619e457cd603012014535f8851184c0fbecdf2751ff95"
     elsif OS.mac?
       root_url "https://github.com/aiurovet/xnx/raw/master/app/macOS"
-      sha256 big_sur: "bc3051c69dc1ede23c31b50de8a8831b987fe33d408ea781e1d97b867a727c3b"
+      sha256 big_sur: "9829601feda4401a92f5587cfa6e9f7b40b1e4f71e6152a3934bca7ba9f3309a"
     end
   end
 
-  # def install
-  #   prefix.install #{HOMEBREW_PREFIX}/Cellar/xnx/0.1.0
-  #   system "cd", "#{HOMEBREW_PREFIX}/Cellar/xnx/0.1.0"
-  #   system "tar", "xvfz", "./xnx-0.1.0-linux-amd64.tar.gz"
-  #   system "rm", "-f", "./xnx-0.1.0-linux-amd64.tar.gz"
-  # end
-
-  # test do
-  #   system "#{HOMEBREW_PREFIX}/Cellar/xnx/0.1.0/xnx", "--version"
-  # end
+  def install
+    ln_s "xnx", "/usr/bin/xnx"
+  end
 end
