5d4e69207685a0dd6ddc7e196040929af38b6c19
aiurovet <aiurovet@gmail.com>
2023-05-06 23:32:19 +1000

    WIP

--- xnx.rb
+++ xnx.rb
@@ -1,15 +1,16 @@
 class Xnx < Formula
   desc "Command-line utility for sophisticated search and replace followed by calling external executables"
   homepage "https://aiurovet.com/applications/xnx.html"
+  url "https://github.com/aiurovet/xnx/raw/"
   version "0.1.0"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/aiurovet/xnx/raw/master/app/bottles"
-
     if OS.linux?
-      sha256 x86_64_linux: "a740e2d73d0e41bda0513a6d002d7babdbf80d4cca1b6d6d63620163444ac6ba"
+      root_url "https://github.com/aiurovet/xnx/raw/master/app/Linux"
+      sha256 x86_64_linux: "6af1847d443fbecc210619e457cd603012014535f8851184c0fbecdf2751ff95"
     elsif OS.mac?
+      root_url "https://github.com/aiurovet/xnx/raw/master/app/macOS"
       sha256 big_sur: "bc3051c69dc1ede23c31b50de8a8831b987fe33d408ea781e1d97b867a727c3b"
     end
   end
